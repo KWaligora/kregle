139117db6cc68b7739a28c5d35ab4b2aa5c1cb0c
wdd246 <adammalota999@interia.eu>
2018-07-11 13:22:39 +0200

    ServiceWorker

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,25 @@
-var CACHE_NAME = "gih-cache-v4";
+var CACHE_NAME = "gih-cache-v5";
 var CACHED_URLS = [
   "index.html",
   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
+  "https://fonts.googleapis.com/css?family=Poppins:100,400",
+  "https://fonts.gstatic.com/s/poppins/v5/pxiGyp8kv8JHgFVrLPTucXtAKPY.woff2",
+  "https://fonts.googleapis.com/css?family=Dancing+Script",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
+  "css/style.css",
+  "css/mobile.css",
+  "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",
+  "http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js",
+  "img/icon.png",
+  "img/bowling.png",
+  "img/logo.png",
+  "img/up.png",
+  "img/facebook.png",
+  "js/index.js",
+  "img/1.jpg",
+  "img/2.jpg",
 ];
 
 self.addEventListener("install", function(event) {
